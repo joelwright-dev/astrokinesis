da435854a4a87f698cd8670ee9f136c0b8eeb93a
Isaac-Liggett <isaacliggett19@protonmail.com>
2024-08-17 07:17:49 +1000

    fixed player jumping and moving glitches

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -59,6 +59,11 @@ public class PlayerController : MonoBehaviour
         isGrounded = true;
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         isGrounded = false;
