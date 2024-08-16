021ffc274041e9a81199e1737ea76f629c1e567d
Isaac-Liggett <isaacliggett19@protonmail.com>
2024-08-16 23:22:08 +1000

    Added player collision. Fixed jumping animation (also added rigidbody forces)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,19 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] public float playerSpeed = 0.025f;
+    [SerializeField] public float jumpForce = 0.025f;
     public Animator animator;
     private SpriteRenderer spriteRenderer;
     private bool isFacingRight = true;
+    public bool isGrounded = false;
+    private Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,21 +26,41 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            this.isFacingRight = true;
             transform.Translate(new Vector3(playerSpeed, 0f, 0f));
-            animator.SetBool("isRunning", true);
-        }else if (Input.GetKey(KeyCode.A)){
+            this.isFacingRight = true;
+            if (isGrounded )
+            {
+                animator.SetBool("isRunning", true);
+            }
+        }
+        if (Input.GetKey(KeyCode.A)){
             this.isFacingRight = false;
             transform.Translate(new Vector3(-playerSpeed, 0f, 0f));
-            animator.SetBool("isRunning", true);
-        }else if (Input.GetKey(KeyCode.W))
-        {
-            animator.SetBool("isGrounded", false);
+            if (isGrounded)
+            {
+                animator.SetBool("isRunning", true);
+            }
         }
-        else
+        if (Input.GetKey(KeyCode.W) && isGrounded)
+        {
+            this.rigidBody.AddForce(new Vector3(0f, jumpForce, 0f));
+            isGrounded = false;
+        }
+        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
             animator.SetBool("isRunning", false);
         }
         this.spriteRenderer.flipX = !isFacingRight;
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
